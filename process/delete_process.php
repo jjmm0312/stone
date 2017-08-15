e8f5ef6b1ec41608b7709f1c89d162b547daaeca
kim jimin <198032@naver.com>
2017-08-15 18:33:43 +0900

    delete

--- process/delete_process.php
+++ process/delete_process.php
@@ -3,10 +3,7 @@
 $conn=mysqli_connect("localhost","root","mango");
 mysqli_select_db($conn,"mango");
 
-$sql = "UPDATE `mango`.`device` SET `name` = '".$_POST['name']."', `description` = '".$_POST['description']."' WHERE `device`.`id` =".$_POST['id'].";";
-$sql = "DELETE FROM `mango`.`device` WHERE `device`.`id`='".$_GET['id']."';";
+$sql = "DELETE FROM `mango`.`device` WHERE `device`.`id`=".$_GET['id'].";";
 $result = mysqli_query($conn, $sql);
 header('Location: /index.php?page=manage');
 ?>
-
-DELETE FROM `mango`.`device` WHERE `device`.`id` = 28;
