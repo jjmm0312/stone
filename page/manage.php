ced138e7547f4bb5865457554e7b8201f2403cee
jisu An <ajs7270@naver.com>
2017-08-15 21:32:54 +0900

    fix delete button

--- page/manage.php
+++ page/manage.php
@@ -25,7 +25,7 @@ while($row=mysqli_fetch_assoc($result)){
   echo   $row['status']==1 ? 'On' : 'Off';
   echo  '<br>추가시간 : '.$row['created'];
   echo  '<br><input type="submit" name="submit" value="변경">';
-  echo  '<br><input type="button" name="submit" value="삭제" onclick="location.href=process/delete_process.php?id='.$row['id'].'>';
+  echo  '<button type="button" name="submit" onclick="location.href=\'process/delete_process.php?id='.$row['id'].'\'">삭제</button><br>';
   echo '</form>';
 }
 
