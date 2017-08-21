1eb1bf5dcd4a537ba54213a2df7ce254e5ba06f5
owlsogul <owlsogul@naver.com>
2017-08-22 02:28:50 +0900

    MySQL 포트 수정 < 추후에 conf 파일 만들기로

--- process/reg_process.php
+++ process/reg_process.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn=mysqli_connect("localhost","root","mango");
+$conn=mysqli_connect("localhost:3307","root","mango");
 mysqli_select_db($conn,"mango");
 $sql = "INSERT INTO `mango`.`device` (`id`, `type`, `name`, `description`, `created`, `status`) VALUES (NULL, '-1','".$_POST['name']."','".$_POST['description']."',now(), '-1');";
 $result = mysqli_query($conn, $sql);
