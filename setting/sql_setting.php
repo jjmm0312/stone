245f5d3cc8008152774cd37c508dddb02318cf94
owlsogul <owlsogul@naver.com>
2017-08-22 03:03:17 +0900

    fix ip crush

--- setting/sql_setting.php
+++ setting/sql_setting.php
@@ -1,11 +1,13 @@
 <?php
 
 $db_ip = 'localhost';
-$db_port = '3307';
+//when using variable of port, raspberry pi server could not connet my sql.
+//so, removing port.
+//$db_port = '3307';
 $db_id = 'root';
 $db_pw = 'mango';
 $db_database = 'mango';
 
-$conn = mysqli_connect($db_ip.":".$db_port, $db_id, $db_pw);
+$conn = mysqli_connect($db_ip, $db_id, $db_pw);
 mysqli_select_db($conn,$db_database);
 ?>
