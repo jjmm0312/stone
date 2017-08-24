4f13214100780bd510d54674994df074a4d12dc1
owlsogul <owlsogul@naver.com>
2017-08-25 00:19:34 +0900

    change password form from normal text field to password text field

--- page/login.php
+++ page/login.php
@@ -1,6 +1,6 @@
 <h1>안전한 보호를 위한 비밀번호 입력하기</h1>
 <form class="login" action="/process/login_process.php" method="post">
   비밀번호 입력</br>
-  <input type="text" name="password" value="" placeholder="type your password"></br>
+  <input type="password" name="password" value="" placeholder="type your password"></br>
   <input type="submit" value="로그인">
 </form>
