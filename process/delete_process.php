7eadb1d36a79639e85469f8201c57582c1404f4c
jisu An <ajs7270@naver.com>
2017-11-03 11:09:04 +0900

    fix process

--- process/delete_process.php
+++ process/delete_process.php
@@ -1,8 +1,11 @@
 <?php
 
-include '/setting/sql_setting.php';
+#include '/setting/sql_setting.php';
 
-$sql = "DELETE FROM `mango`.`device` WHERE `device`.`id`=".$_GET['id'].";";
+$conn =mysqli_connect("localhost","root","mango");
+mysqli_select_db($conn,"mango");
+
+$sql = "DELETE FROM `mango`.`device` WHERE `device`.`id` = '".$_POST['id']."';";
 $result = mysqli_query($conn, $sql);
 header('Location: /index.php?page=manage');
 ?>
